4ba00b771d1da54d984e047fadd3ce79969a4a0a
saad0104 <saad.csecu@gmail.com>
2025-02-04 16:35:24 +0600

    .

---
+++ 628_Maximum_product_of_three_numbers.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int n = nums.size() - 1;
+
+sort(nums.begin(), nums.end());
+        return  max((nums[n] *nums[n - 1] * nums[n - 2]),(nums[n] * nums[0] * nums[1]));
+    }
+};
